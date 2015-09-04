9fc78b25f5a2dd30549500eb024e54fbe29067e4
Chris Rees <serneum@gmail.com>
2015-09-03 23:27:40 -0400

    Add ball reset after points are scored

--- ball.js
+++ ball.js
@@ -2,18 +2,20 @@ function Ball(canvas) {
     this.canvas = canvas;
     this.canvasContext = this.canvas.getContext('2d');
     this.radius = 10;
-
-    // Start in the middle of the canvas
-    this.x = (this.canvas.width / 2) - this.radius;
-    this.y = (this.canvas.height / 2) - this.radius;
-
-    // Random starting direction/angle
-    var angle = 0; //getRandomAngle();
-    this.velX = getRandomVelocity() * Math.cos(angle);
-    this.velY = getRandomVelocity() * Math.sin(angle);
+    this.reset();
 };
 
 Ball.prototype = {
+    reset: function() {
+        // Start in the middle of the canvas
+        this.x = (this.canvas.width / 2) - this.radius;
+        this.y = (this.canvas.height / 2) - this.radius;
+
+        // Random starting direction/angle
+        var angle = getRandomAngle();
+        this.velX = getRandomVelocity() * Math.cos(angle);
+        this.velY = getRandomVelocity() * Math.sin(angle);
+    },
     move: function() {
         this.x += this.velX;
         this.y += this.velY;
@@ -26,12 +28,12 @@ Ball.prototype = {
         this.canvasContext.fill();
     },
     bounce: function() {
-        if (this.x >= this.canvas.width || this.x <= 0) {
-            this.bounceVelX();
-        }
-
         if (this.y >= this.canvas.height || this.y <= 0) {
             this.bounceVelY();
+        }
+
+        if (this.x < 0 || this.x > this.canvas.width) {
+            this.reset();
         }
     },
     bounceVelX: function() {
